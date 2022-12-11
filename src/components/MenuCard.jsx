12039d5cce79d7b4703aa0fe32d3144b5c979b3b
Cililocwee <corrie.stroup@gmail.com>
2022-12-11 16:13:18 +0700

    added lazy loading to menu page

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import LazyLoad from "react-lazy-load";
 import DrinkCard from "./DrinkCard";
 
 export default function MenuCard({ viewName, viewArr }) {
   return (
-    <div className="menu-wrapper">
-      {viewArr.map((item) => {
-        return (
-          <DrinkCard
-            type={item.type}
-            description={item.description}
-            image={item.picture}
-            price={item.price}
-            id={item.id}
-          />
-        );
-      })}
-    </div>
+    <LazyLoad>
+      <div className="menu-wrapper">
+        {viewArr.map((item) => {
+          return (
+            <DrinkCard
+              type={item.type}
+              description={item.description}
+              image={item.picture}
+              price={item.price}
+              id={item.id}
+            />
+          );
+        })}{" "}
+      </div>
+    </LazyLoad>
   );
 }
--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -5,8 +5,6 @@ import MenuLink from "../components/MenuLink";
 import coffees from "../menuitems/coffeeData";
 import foods from "../menuitems/foodData";
 import teas from "../menuitems/teaData";
-import MenuBG from "../assets/menubackground.jpg";
-import BackDrop from "../components/BackDrop";
 
 export default function MenuPage() {
   const all = [...coffees, ...foods, ...teas];
